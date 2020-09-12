ef19bbde908c27e9ed24168b69a83bb0ca7d7089
Marvin T <contact@marvinkturner.com>
2020-09-11 23:58:06 -0400

    feat: add js code to get media streams

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
 // let corsProxyServer = https://gentle-plains-15721.herokuapp.com/ ;
 
-console.log('testing')
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
+
+// Pass selected media strem to video element, then play
+async function selectMediaStream() {
+    try {
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+        videoElement.srcObject = mediaStream;
+        videoElement.onloadedmetadata = () => {
+            videoElement.play();
+        }
+    } catch (error) {
+        console.log(`Whoopsie: ${error}`);
+    }
+}
+
+button.addEventListener('click', async () => {
+    // Disable button
+    button.disabled = true;
+    // Start PiP
+    await videoElement.requestPictureInPicture();
+    // Reset Button
+    button.disabled = false;
+});
+
+// On Load
+selectMediaStream();
